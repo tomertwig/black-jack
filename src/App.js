ca50e1c9bb945512d91518495deb22b1e0c22924
Tomer <tomert@dropbox.com>
2018-12-23 15:19:07 +0200

    .

--- src/App.js
+++ src/App.js
@@ -1,28 +1,24 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './app.css'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+class AppCls extends React.Component {
+		
+	constructor() {
+	  super()
+	  this.state = {
+	  }
+	}
+
+	onClickHandler = (e) => {
+		console.log('clicked');
+	}
+	render(){
+		return <div className='container'>
+                <button className='start-game-button' onClick={this.onClickHandler}>
+								Start Game 
+								</button> 
+						</div>; 	
+		}
 }
 
-export default App;
+export {AppCls};
