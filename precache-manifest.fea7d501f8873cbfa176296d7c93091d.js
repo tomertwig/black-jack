96d87f11dc041f3b1c8f227af1ea3032d45832d8
Tomer <tomert@dropbox.com>
2018-12-31 17:52:26 +0200

    Updates

---
+++ precache-manifest.fea7d501f8873cbfa176296d7c93091d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "036868daadf00095ce944411bfe2f2e3",
+    "url": "/black-jack/static/media/blackjack_table_layout.036868da.svg"
+  },
+  {
+    "revision": "54b48005ec8558f66cd3f0048b9f315a",
+    "url": "/black-jack/static/media/blackjack_logo.54b48005.png"
+  },
+  {
+    "revision": "51c9dac2940464658203267ca252029b",
+    "url": "/black-jack/static/media/Dropbox_Logo.51c9dac2.png"
+  },
+  {
+    "revision": "de351821a5ed7651416b",
+    "url": "/black-jack/static/js/runtime~main.de351821.js"
+  },
+  {
+    "revision": "d1f5b69f7e1fde3cb9a5",
+    "url": "/black-jack/static/js/main.d1f5b69f.chunk.js"
+  },
+  {
+    "revision": "701e80665e0aa5267d43",
+    "url": "/black-jack/static/js/1.701e8066.chunk.js"
+  },
+  {
+    "revision": "d1f5b69f7e1fde3cb9a5",
+    "url": "/black-jack/static/css/main.222e649a.chunk.css"
+  },
+  {
+    "revision": "3150820c909a75776f0d92f544d3d210",
+    "url": "/black-jack/index.html"
+  }
+];
